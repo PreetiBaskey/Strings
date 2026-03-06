aac4fc33871ebd700e546f8714d4ca80f6fde994
PreetiBaskey <38905783+PreetiBaskey@users.noreply.github.com>
2026-03-06 13:10:18 +0530

    leetcode easy check if binary string has at most one segment of ones

---
+++ CheckIfBinaryStringHasAtMostOneSegmentOfOnes.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    bool checkOnesSegment(string s) {
+        // Since the string has no leading zeros, it starts with '1'.
+        // If we find "01", it means there is at least a second segment of ones.
+        return s.find("01") == string::npos;
+    }
+};
